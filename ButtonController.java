5f7bea28e86a8dd5a8579623b055306fdd86bb42
Dalton Pyle <Dalton.w.pyle@ou.edu>
2019-10-05 11:01:19 -0500

    started streamlining the gui initilationz process

--- ButtonController.java
+++ ButtonController.java
@@ -13,13 +13,53 @@ public class ButtonController extends Frame implements ActionListener  {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		JFrame[] windows = calc.getwindowArray();
-		windows[1].setVisible(true);
-		windows[0].setVisible(false);
+		
+		int currentWindow = calc.getCurrentWindow();
+		switch (currentWindow){
+		case 0:
+			startMenue(e);
+			break;
+		case 1:
+			heatTransfer(e);
+			break;
+		case 2: massTransfer(e);
+			break;
+		}
+		
 		
 	}
 	public JButton getButton() {
 		return button;
+	}
+	private void startMenue(ActionEvent e) {
+		JFrame[] windows = calc.getwindowArray();
+		final int currentWindow = 0;
+		final int heatTransfer = 1;
+		final int massTransfer = 2;
+		if(e.getActionCommand().equals("Heat Transfer")) {
+			windows[heatTransfer].setLocation(windows[currentWindow].getLocation());
+			windows[heatTransfer].setVisible(true);
+			calc.setCurrentWindwo(1);
+			windows[currentWindow].setVisible(false);
+		}else {
+			windows[massTransfer].setLocation(windows[currentWindow].getLocation());
+			windows[massTransfer].setVisible(true);
+			calc.setCurrentWindwo(2);
+			windows[currentWindow].setVisible(false);
+		}
+		
+	}private void heatTransfer(ActionEvent e) {
+		JFrame[] windows = calc.getwindowArray();
+		
+	}
+	private void massTransfer(ActionEvent e) {
+		JFrame[] windows = calc.getwindowArray();
+		final int startMenue = 0;
+		final int currentWindow = 2;
+		windows[startMenue].setLocation(windows[currentWindow].getLocation());
+		windows[startMenue].setVisible(true);
+		calc.setCurrentWindwo(0);
+		windows[currentWindow].setVisible(false);
 	}
 	
 }
--- MoHeatCalculator.java
+++ MoHeatCalculator.java
@@ -4,38 +4,74 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MoHeatCalculator extends JFrame {
-	private String[] buttonNames = {"Heat Transfer","Mass Transfer","Steady State","Un Steady State"};
+	private String[] buttonNames = {"Heat Transfer","Mass Transfer","Steady State","Un Steady State","Go Back"};
 	private String[] textFeildNames;
-	private String[] windowNames = {"Start menue","Transfer selection","Heat Transfer"};
+	private String[] windowNames = {"Start menue","Heat Transfer","MassTransfer"};
 	private ButtonController[] buttons = new ButtonController[buttonNames.length];
 	private JFrame[] windows = new JFrame[3] ;
 	private MoHeatCalculator calc;
+	private int currentWindow;
+	/*rangOfButtonOnWindow is also an index: first window has 2 buttons second window has 2 buttons for the second index of range 
+	we must add the previous number of buttons to it {n,n+buttons on fram 2,n +buttons on frame 3} and so on*/
+	private int[] rangeOfButtonsOnWindow = {2,4,5};
 	public void GuiInitilizer() {
-		//inlizing all frames
 		
 		
+		/**********************************************************************************************
+		 * this is Initlization for the array containning every window
+		 */
 		for (int i = 0; i < windows.length; ++i) {
 			windows[i] = new JFrame();
 			windows[i].setSize(400,400);
 			windows[i].setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 			windows[i].setLayout(null);
-		}//first frame gets 2 buttons
-		for(int i = 0; i < 2; ++i) {
+		}
+		/**********************************************************************************************
+		 * this is Initilazation for the Start menue it adds buttons and labels accordingly
+		 */
+		for(int i = 0; i < rangeOfButtonsOnWindow[0]; ++i) {
 			int xpos = 130;
 			int ypos = 40;
 			int widith = 100;
 			int height = 50;
 			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i],calc);
 			windows[0].add(buttons[i].getButton());
-		}for (int i = 2; i < 4; ++i) {
+		}
+		/********************************************************************************************
+		 * this is Initiliazation for the Heat Transfer Window it adds buttons and labels accordingly
+		 */
+		/*for (int i = rangeOfButtonsOnWindow[0]; i < rangeOfButtonsOnWindow[1]; ++i) {
 			int xpos = 130;
 			int ypos = 40;
 			int widith = 100;
 			int height = 50;
 			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i],calc);
 			windows[1].add(buttons[i].getButton());
+		}*/
+		buttonInitilizer(130,40,60,60,100,50,"Heat Transfer","Start menue",1,2);
+		
+		
+		/******************************************************************************************
+		 * this is initiliazation for the Mass Transfer Window it adds buttons and labels accordingly
+		 */
+		
+		for (int i = rangeOfButtonsOnWindow[1]; i <rangeOfButtonsOnWindow[2]; ++i) {
+			buttons[i] = new ButtonController(130, 40, 100, 50, "back", calc);
+			windows[2].add(buttons[i].getButton());
 		}
+		JLabel lable = new JLabel("This feature is comming soon");
+		lable.setBounds(100, 200, 200, 40);
+		windows[2].add(lable);
+		
+		
+		
+		/*********************************************************************************************
+		 * this sets up the SteadState heatTransferWindow
+		 */
+		
+		
 		windows[0].setVisible(true);
+		currentWindow = 0;
 		
 		
 	}
@@ -50,6 +86,34 @@ public class MoHeatCalculator extends JFrame {
 		return windows;
 	}private void setCalculatorObject(MoHeatCalculator calc) {
 		this.calc = calc;
+	}public int getCurrentWindow() {
+		return currentWindow;
+	}public void setCurrentWindwo(int currentWindow) {
+		this.currentWindow = currentWindow;
+	}private void buttonInitilizer(int xIni,int yIni, int xSpacing,int ySpacing,int height,int widith ,String windowName,String perviousWindow,int rows,int columns) {
+		int desieredWindow = -1;
+		int oldWindow = -1;
+		for (int i = 0; i < windowNames.length; ++i) {
+			if (perviousWindow.equals(windowNames[i])) {
+				oldWindow = i;
+			}else if (windowName.equals(windowNames[i])) {
+				desieredWindow = i;
+				
+			}
+			
+		}
+		
+		int currentColumn = 1 ,currentRow = 1, xpos = xIni, ypos = yIni;
+		for (int i = rangeOfButtonsOnWindow[oldWindow], k = 0; i < rangeOfButtonsOnWindow[desieredWindow]; ++i , ++k) {
+			buttons[i] = new ButtonController(xpos,ypos,widith,height,buttonNames[i],calc);
+			windows[desieredWindow].add(buttons[i].getButton());
+			if (currentColumn != columns) {
+				xpos = xSpacing * k + xIni;
+			}else if (currentRow != rows) {
+				ypos = ySpacing * k + yIni;
+				columns = 1;
+			}
+		}
 	}
 
 	
