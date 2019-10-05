9f8d4fa200330c5290dcf49eb8badd48a494c765
Dalton Pyle <Dalton.w.pyle@ou.edu>
2019-10-05 00:01:19 -0500

    started work on GUI

--- ButtonController.java
+++ ButtonController.java
@@ -2,10 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class ButtonController extends JFrame implements ActionListener{
+public class ButtonController extends MoHeatCalculator implements ActionListener  {
+	private JButton button;
+	public ButtonController(int xPos, int yPos, int widith, int height,String name) {
+		this.button = new JButton(name);
+		this.button.setBounds(xPos,yPos,widith,height);
+		this.button.addActionListener(this);
+	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
+		getWindowsArray()[0].setVisible(false);
+		getWindowsArray()[1].setVisible(true);
+	}
+	public JButton getButton() {
+		return button;
 	}
 	
 }
--- MoHeatCalculator.java
+++ MoHeatCalculator.java
@@ -4,20 +4,48 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MoHeatCalculator extends JFrame implements ActionListener{
-	private String[] buttonNames = {"Heat Transfer","Mass Transfer"};
+	private String[] buttonNames = {"Heat Transfer","Mass Transfer","Steady State","Un Steady State"};
 	private String[] textFeildNames;
 	private String[] windowNames = {"Start menue","Transfer selection","Heat Transfer"};
-	private ButtonController[] buttons;
-	private JPanel[] windows;
-	
-	public  GuiInitilizer() {
+	private ButtonController[] buttons = new ButtonController[buttonNames.length];
+	private JFrame[] windows = new JFrame[3] ;
+	public void GuiInitilizer() {
+		//inlizing all frames
 		
+		
+		for (int i = 0; i < windows.length; ++i) {
+			windows[i] = new JFrame();
+			windows[i].setSize(400,400);
+		}//first frame gets 2 buttons
+		for(int i = 0; i < 2; ++i) {
+			int xpos = 130;
+			int ypos = 40;
+			int widith = 100;
+			int height = 50;
+			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i]);
+			windows[0].add(buttons[i].getButton());
+		}for (int i = 2; i < 4; ++i) {
+			int xpos = 130;
+			int ypos = 40;
+			int widith = 100;
+			int height = 50;
+			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i]);
+			windows[2].add(buttons[i].getButton());
+		}
+		windows[0].setLayout(null);
+		windows[0].setVisible(true);
 	}
 	public void actionPerformed(ActionEvent e) {
 		
 	}
 	public static void main(String [] args) {
+		MoHeatCalculator cal = new MoHeatCalculator();
+		cal.GuiInitilizer();
 		
+		
+	}
+	public JFrame[] getWindowsArray(){
+		return windows;
 		
 	}
 	
