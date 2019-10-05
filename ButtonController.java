b7262ea6b356c65fbf66945d55b4d7e3975d21c1
Dalton Pyle <Dalton.w.pyle@ou.edu>
2019-10-04 22:00:16 -0500

    initilaziation

---
+++ ButtonController.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ButtonController extends JFrame implements ActionListener{
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+}
---
+++ MoHeatCalculator.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class MoHeatCalculator extends JFrame implements ActionListener{
+	private String[] buttonNames = {"Heat Transfer","Mass Transfer"};
+	private String[] textFeildNames;
+	private String[] windowNames = {"Start menue","Transfer selection","Heat Transfer"};
+	private ButtonController[] buttons;
+	private JPanel[] windows;
+	
+	public  GuiInitilizer() {
+		
+	}
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	public static void main(String [] args) {
+		
+		
+	}
+	
+}
