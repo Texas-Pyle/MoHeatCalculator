6dbe5c41403648eeebba5362647bb204070daff9
Dalton Pyle <Dalton.w.pyle@ou.edu>
2019-10-05 09:01:06 -0500

    finished multi fram implimentation

--- ButtonController.java
+++ ButtonController.java
@@ -2,17 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class ButtonController extends MoHeatCalculator implements ActionListener  {
+public class ButtonController extends Frame implements ActionListener  {
 	private JButton button;
-	public ButtonController(int xPos, int yPos, int widith, int height,String name) {
+	private MoHeatCalculator calc;
+	public ButtonController(int xPos, int yPos, int widith, int height,String name,MoHeatCalculator calc ) {
 		this.button = new JButton(name);
 		this.button.setBounds(xPos,yPos,widith,height);
 		this.button.addActionListener(this);
+		this.calc = calc;
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		getWindowsArray()[0].setVisible(false);
-		getWindowsArray()[1].setVisible(true);
+		JFrame[] windows = calc.getwindowArray();
+		windows[1].setVisible(true);
+		windows[0].setVisible(false);
+		
 	}
 	public JButton getButton() {
 		return button;
--- MoHeatCalculator.java
+++ MoHeatCalculator.java
@@ -3,12 +3,13 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-public class MoHeatCalculator extends JFrame implements ActionListener{
+public class MoHeatCalculator extends JFrame {
 	private String[] buttonNames = {"Heat Transfer","Mass Transfer","Steady State","Un Steady State"};
 	private String[] textFeildNames;
 	private String[] windowNames = {"Start menue","Transfer selection","Heat Transfer"};
 	private ButtonController[] buttons = new ButtonController[buttonNames.length];
 	private JFrame[] windows = new JFrame[3] ;
+	private MoHeatCalculator calc;
 	public void GuiInitilizer() {
 		//inlizing all frames
 		
@@ -16,37 +17,40 @@ public class MoHeatCalculator extends JFrame implements ActionListener{
 		for (int i = 0; i < windows.length; ++i) {
 			windows[i] = new JFrame();
 			windows[i].setSize(400,400);
+			windows[i].setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			windows[i].setLayout(null);
 		}//first frame gets 2 buttons
 		for(int i = 0; i < 2; ++i) {
 			int xpos = 130;
 			int ypos = 40;
 			int widith = 100;
 			int height = 50;
-			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i]);
+			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i],calc);
 			windows[0].add(buttons[i].getButton());
 		}for (int i = 2; i < 4; ++i) {
 			int xpos = 130;
 			int ypos = 40;
 			int widith = 100;
 			int height = 50;
-			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i]);
-			windows[2].add(buttons[i].getButton());
+			buttons[i] = new ButtonController(xpos,ypos + 60 * i,widith,height,buttonNames[i],calc);
+			windows[1].add(buttons[i].getButton());
 		}
-		windows[0].setLayout(null);
 		windows[0].setVisible(true);
-	}
-	public void actionPerformed(ActionEvent e) {
+		
 		
 	}
+	
 	public static void main(String [] args) {
 		MoHeatCalculator cal = new MoHeatCalculator();
+		cal.setCalculatorObject(cal);
 		cal.GuiInitilizer();
 		
 		
-	}
-	public JFrame[] getWindowsArray(){
+	}public JFrame[] getwindowArray() {
 		return windows;
-		
+	}private void setCalculatorObject(MoHeatCalculator calc) {
+		this.calc = calc;
 	}
+
 	
 }
